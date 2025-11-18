161f2d2a6871b3d2cb225ee5a0dfcee8c52a2dc6
Luísa Corrêa <36203540+LuisaFC@users.noreply.github.com>
2025-11-18 22:55:50 +0000

    Change construction page message

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,35 @@
 export default function Home() {
-  return <h1> Initial Page!</h1>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        minHeight: "100vh",
+        padding: "20px",
+        boxSizing: "border-box",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: "800px",
+          width: "100%",
+          textAlign: "center",
+          marginBottom: "20px",
+          lineHeight: "20px",
+        }}
+      >
+        <h1 style={{ color: "#35373b" }}>💪🏼 Em Construção 💪🏼</h1>
+        <p style={{ color: "#414347" }}>
+          Estamos nesse exato momento construindo um local para quem busca
+          <strong> motivação diária</strong> e para quem quer compartilhar seu{" "}
+          <strong>progesso</strong> nas atividades físicas. Aqui é um{" "}
+          <strong>lugar seguro</strong> para você compartilhar seus
+          aprendizados, rotina, motivação, evolulção. O que é fácil para uns
+          pode ser dificil para outros! Não deixe de compartilhar e ajude a
+          crescer nossa comunidade 💙 💫 🔥 🎧 🔋
+        </p>
+      </div>
+    </div>
+  );
 }
